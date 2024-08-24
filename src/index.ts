20db00791198f5d41ec68a364924ad90e908f91f
Jijo James <jijojames18@gmail.com>
2024-08-24 21:13:51 +0530

    Add authentication

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,59 @@
 import express from "express";
 import cors from "cors";
 
-import gql from "graphql-tag";
-import { ApolloServer } from "@apollo/server";
-import { buildSubgraphSchema } from "@apollo/subgraph";
-import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServer } from "apollo-server-express";
 import { readFileSync } from "fs";
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import { resolvers } from "./resolvers.js";
 
 const PORT = process.env.PORT || 5050;
+const JWT_SECRET = Buffer.from(process.env.SECRET, "base64");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const typeDefs = gql(
-  readFileSync("./src/schema.graphql", {
-    encoding: "utf-8",
+app.use(
+  expressjwt({
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    secret: JWT_SECRET,
   })
 );
 
-const server = new ApolloServer({
-  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+app.post("/login", async (req, res) => {
+  // In implementing project, validate the details here
+  // const { email, password } = req.body;
+  const token = jwt.sign({ sub: "user" }, JWT_SECRET);
+  res.json({ token });
+  //res.sendStatus(401);
 });
+
+const typeDefs = readFileSync("./src/schema.graphql", {
+  encoding: "utf-8",
+});
+
+const context = async ({ req }) => {
+  if (req.auth) {
+    const user = {
+      user: req.auth.sub,
+    };
+    return { user };
+  }
+  return {};
+};
+
+const server = new ApolloServer({ typeDefs, resolvers, context });
 
 await server.start();
 
-app.use("/graphql", cors(), express.json(), expressMiddleware(server));
+server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -8,18 +8,27 @@ import {
   queryUserByEmailAfterUpdate,
 } from "./db/index.js";
 
+const checkIfValidUser = (user) => {
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+  return true;
+};
+
 const resolvers = {
   Query: {
-    async users(parent, { type, needle }) {
-      if (!needle && !type) {
-        return await fetchAllUsers();
-      } else if (type === "email") {
-        return await searchUserByEmail(needle);
-      } else if (type === "phone_number") {
-        return await searchUserByNumber(needle);
-      }
+    async users(parent, { type, needle }, { user }) {
+      if (checkIfValidUser(user)) {
+        if (!needle && !type) {
+          return await fetchAllUsers();
+        } else if (type === "email") {
+          return await searchUserByEmail(needle);
+        } else if (type === "phone_number") {
+          return await searchUserByNumber(needle);
+        }
 
-      return [];
+        return [];
+      }
     },
   },
   User: {
@@ -27,17 +36,23 @@ const resolvers = {
     createdAt: (parent) => parent["created_at"],
   },
   Mutation: {
-    async createUser(parent, { name, email, phoneNumber }) {
-      await insertUser(email, name, phoneNumber);
-      return await queryUserByEmailAfterUpdate(email);
+    async createUser(parent, { name, email, phoneNumber }, { user }) {
+      if (checkIfValidUser(user)) {
+        await insertUser(email, name, phoneNumber);
+        return await queryUserByEmailAfterUpdate(email);
+      }
     },
-    async updateUser(parent, { name, email, phoneNumber }) {
-      await updateUser(email, name, phoneNumber);
-      return await queryUserByEmailAfterUpdate(email);
+    async updateUser(parent, { name, email, phoneNumber }, { user }) {
+      if (checkIfValidUser(user)) {
+        await updateUser(email, name, phoneNumber);
+        return await queryUserByEmailAfterUpdate(email);
+      }
     },
-    async deleteUser(parent, { email }) {
-      await deleteUser(email);
-      return true;
+    async deleteUser(parent, { email }, { user }) {
+      if (checkIfValidUser(user)) {
+        await deleteUser(email);
+        return true;
+      }
     },
   },
 };
